36dd39e87e48cc040d13f8d9745f0fb809c34123
Ivan Jauregui <uruaf571@gmail.com>
2024-01-31 11:36:16 -0800

    removing unused code

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,6 @@ public class Main {
     public static void main (String[]args) {
 
         System.out.println("My calculator") ;
-        System.out.println("My calculator") ;
-        System.out.println("My calculator") ;
-        System.out.println("My calculator") ;
-        System.out.println("My calculator") ;
 
         Main ivan = new Main() ;
         Main manuel = new Main() ;
