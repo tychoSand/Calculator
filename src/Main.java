46dfa2efe96b190d961ee524d18b570dea8bd415
manue <dam2600@gmail.com>
2024-01-31 12:14:30 -0800

    adding functional operation to Scanner local method.

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 //Calculator Jauregui 0.1
 public class Main {
     public int test = 0 ;
-    public double number1 = 0;
-    public double number2 = 0;
+    public static double number1 = 0;
+    public static double number2 = 0;
 
 
     public static void main (String[]args) {
 
-        testingCode(); //ddsr
+//        testingCode(); //ddsr
+        scannerNumbers (); // DDSR
     }
 
     // sum method
-    protected double sum(double x, double y) {
+    protected static double sum(double x, double y) {
         return x + y ;
     }
     // subtract method
@@ -32,10 +33,16 @@ public class Main {
     protected double percentage(double x, double y) {
         return x % y ;
     }
-    protected void scannerNumbers () { //ddsr
+    protected static void scannerNumbers () { //ddsr
         Scanner scan = new Scanner (System.in);
         System.out.println("Dame tu primer numero: ");
         number1 = scan.nextDouble();
+        System.out.println("Dame tu segundo numero: ");
+        number2 = scan.nextDouble();
+
+        double result = sum(number1,number2);
+        System.out.println("el resultado es: "+result);
+
     }
 
     protected static void testingCode () {
