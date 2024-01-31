135610542fc1d0fb08e5eda4cbc45403e007b5d2
manue <dam2600@gmail.com>
2024-01-31 11:52:03 -0800

    moving test code to a static function.

--- src/Main.java
+++ src/Main.java
@@ -9,35 +9,7 @@ public class Main {
 
     public static void main (String[]args) {
 
-        System.out.println("My calculator") ;
-
-        Main ivan = new Main() ;
-        Main manuel = new Main() ;
-    // sum
-        double sumNumbers = ivan.sum(35,88) ;
-        System.out.println("The result of the sum is " + sumNumbers) ;
-
-    // subtract
-        double subtractNumbers = ivan.subtract(25,18) ;
-        System.out.println("The result of the subtract is " + subtractNumbers) ;
-
-    // multiply
-        double multiplyNumbers = ivan.multiply(8,18) ;
-        System.out.println("The result of the multiply is " + multiplyNumbers) ;
-
-    // divide
-        double divideNumbers = ivan.divide(50,9) ;
-        System.out.println("The result of the division is " + divideNumbers) ;
-
-    // percentage
-        double percentageNumbers = ivan.percentage(90,25) ;
-        System.out.println("The result of the percentage is " + percentageNumbers) ;
-
-        ivan.test = 69 ;
-        manuel.test = 28 ;
-
-        System.out.println("The value of the test on ivan " + ivan.test) ;
-        System.out.println("The value of the test on manuel " + manuel.test) ;
+        testingCode();
     }
 
     // sum method
@@ -64,5 +36,37 @@ public class Main {
         Scanner scan = new Scanner (System.in);
         System.out.println("Dame tu primer numero: ");
         number1 = scan.nextDouble();
+    }
+
+    protected static void testingCode () {
+        System.out.println("My calculator") ;
+
+        Main ivan = new Main() ;
+        Main manuel = new Main() ;
+        // sum
+        double sumNumbers = ivan.sum(35,88) ;
+        System.out.println("The result of the sum is " + sumNumbers) ;
+
+        // subtract
+        double subtractNumbers = ivan.subtract(25,18) ;
+        System.out.println("The result of the subtract is " + subtractNumbers) ;
+
+        // multiply
+        double multiplyNumbers = ivan.multiply(8,18) ;
+        System.out.println("The result of the multiply is " + multiplyNumbers) ;
+
+        // divide
+        double divideNumbers = ivan.divide(50,9) ;
+        System.out.println("The result of the division is " + divideNumbers) ;
+
+        // percentage
+        double percentageNumbers = ivan.percentage(90,25) ;
+        System.out.println("The result of the percentage is " + percentageNumbers) ;
+
+        ivan.test = 69 ;
+        manuel.test = 28 ;
+
+        System.out.println("The value of the test on ivan " + ivan.test) ;
+        System.out.println("The value of the test on manuel " + manuel.test) ;
     }
 }
