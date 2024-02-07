3c6f7ce67faac4f7f067be1e3463948ede221149
manue <dam2600@gmail.com>
2024-02-07 12:52:50 -0800

    adding class structure instance operation methods, adding suma only.

--- src/Main.java
+++ src/Main.java
@@ -23,10 +23,10 @@ public class Main {
     }
 
     // sum method
-    protected static double sum(double x, double y) {
-
-        return x + y;
-    }
+//    protected static double sum(double x, double y) {
+//
+//        return x + y;
+//    }
 
     // subtract method
     protected static double subtract(double x, double y) {
@@ -85,7 +85,8 @@ public class Main {
 
         switch (currentOption) {
             case optionSum:
-                result = sum(number1, number2);
+                Suma  variableSuma = new Suma();
+                result = variableSuma.Operation(number1,number2);
                 break;
             case optionSubtract:
                 result = subtract(number1, number2);
