f7838c75a2a8e200a284bab6f25dbe5f9564a5b5
sorm85 <5048930+sorm85@users.noreply.github.com>
2024-05-05 17:27:15 -0700

    adding code for feature from testing repository

--- src/main/java/org/example/KmCalc.java
+++ src/main/java/org/example/KmCalc.java
@@ -1,55 +1,26 @@
 package org.example ;
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class KmCalc { //Km_80
-    public static void main (String [] args) {
-Scanner scanner = new Scanner(System.in) ;
-// Create an object of Scanner class
+    private JButton button1;
+    private JPanel panelMain;
 
- // Enter numbers
-        System.out.println("Welcome human!") ;
-        System.out.println("Enter the first number:") ;
-     double num1 = scanner.nextDouble() ;
-// Enter numbers
-        System.out.println("Enter second number:") ;
-     double num2 = scanner.nextDouble() ;
-// Enter operator
-        System.out.println("Choose operation") ;
-        char operator = scanner.next().charAt(0) ;
-
-     double result ;
-
-   switch (operator) { //Km_80
-       case'+' :
-           result = num1 + num2 ;
-           System.out.println("Result:" + result) ;
-           break ;
-       case'-' :
-           result = num1 - num2 ;
-           System.out.println("Result:" + result) ;
-           break ;
-       case'*' :
-           result = num1 * num2 ;
-           System.out.println("Result:" + result) ;
-           break ;
-       case'/' :
-         if (num2 != 0) {
-             result = num1 / num2 ;
-             System.out.println("Result:" + result) ;
-         } else {
-             System.out.println("Error: Cannot divide by zero!") ;
-         }
-           break ;
-       case'%' :
-           result = num1 % num2 ;
-           System.out.println("Result:" + result) ;
-           break ;
-       default:
-           System.out.println("Error: Invalid operator!");
-
-   }
-
-    scanner.close() ;
+    public KmCalc() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"Hello Guapo");
+            }
+        });
     }
 
+    public static void main (String[] args) {
+        JFrame frame = new JFrame("Cal");
+        frame.setContentPane(new KmCalc().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
