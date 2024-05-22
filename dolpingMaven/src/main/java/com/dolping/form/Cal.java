24a26cc244dc56398a6470a705eb1a2384d75967
Andres Jauregui <uruaf571@gmail.com>
2024-05-22 12:23:12 -0700

    Add information from featuredJauregui to DevelopmentJauregui.

---
+++ dolpingMaven/src/main/java/com/dolping/form/Cal.java
@@ -0,0 +1,26 @@
+package org.example ;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class KmCalc { //Km_80
+    private JButton button1;
+    private JPanel panelMain;
+
+    public KmCalc() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"Hello Guapo");
+            }
+        });
+    }
+
+    public static void main (String[] args) {
+        JFrame frame = new JFrame("Cal");
+        frame.setContentPane(new KmCalc().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
